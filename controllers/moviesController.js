fb04b7aa5477f89529070a9f727de274153819bf
Jalen <jalenms2012@gmail.com>
2024-01-23 16:42:32 -0800

    Added routes for the controller

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,1 +1,15 @@
 const Movie = require('../models/movie')
+
+
+/*
+
+// POST
+router.post('/', movieCtrl.create)
+// GET
+router.get('/', movieCtrl.index)
+// GET
+router.get('/:id', movieCtrl.show)
+// POST
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer)
+
+*/
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -2,4 +2,14 @@ const express = require('express')
 const router = express.Router()
 const movieCtrl = require('../controllers/moviesController')
 
+// POST
+router.post('/', movieCtrl.create)
+// GET
+router.get('/', movieCtrl.index)
+// GET
+router.get('/:id', movieCtrl.show)
+// POST
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer)
+
+
 module.exports = router
